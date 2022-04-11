f461edaced4037c677b3960f43462e5847f457de
Pocari-Eg <66935108+Pocari-Eg@users.noreply.github.com>
2022-04-11 16:40:56 +0900

    test
    test

--- TestProject/Source/TestProject/MyActor.cpp
+++ TestProject/Source/TestProject/MyActor.cpp
@@ -8,7 +8,7 @@ AMyActor::AMyActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	Mesh = CreateDefaultSubobject<StaticMeshComponent>(TEXT("MESH"));
 	RootComponent = Mesh;
 	Comp = CreateDefaultSubobject<UMyActorComponent>(TEXT("Compo"));
 	Comp->Mesh->SetupAttachment(RootComponent);
